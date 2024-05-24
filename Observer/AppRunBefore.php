c1b180d3908d0a32cd4485de3f021e0027910936
Aiweline <32635766+Aiweline@users.noreply.github.com>
2024-05-24 16:24:58 +0800

    Update AppRunBefore.php

--- Observer/AppRunBefore.php
+++ Observer/AppRunBefore.php
@@ -7,6 +7,7 @@ use Weline\Framework\Event\Event;
 use Weline\Framework\Event\ObserverInterface;
 use Weline\Framework\Http\Cookie;
 use Weline\Framework\Manager\ObjectManager;
+use Weline\I18n\Cache\I18nCache;
 use Weline\I18n\Model\Locals;
 
 class AppRunBefore implements ObserverInterface
@@ -16,7 +17,7 @@ class AppRunBefore implements ObserverInterface
      */
     public function execute(Event $event)
     {
-        if(CLI){
+        if (CLI) {
             return;
         }
         $_SERVER['WELINE-USER-LANG'] = Cookie::get('WELINE-USER-LANG');
@@ -29,7 +30,7 @@ class AppRunBefore implements ObserverInterface
             }
             # 排除后端地址和接口隐私地址
             $replace_part = '';
-            $backend_pre = Env::getInstance()->getConfig('admin');
+            $backend_pre  = Env::getInstance()->getConfig('admin');
             if ($backend_pre == $uri_arr[0]) {
                 array_shift($uri_arr);
                 $replace_part = $backend_pre;
@@ -47,16 +48,35 @@ class AppRunBefore implements ObserverInterface
             $lang_path = array_shift($uri_arr);
             # 必须有前两个字符是否都是小写字母,且第三个字符必须是_
             if (strlen($lang_path) > 3 and ctype_lower(substr($lang_path, 0, 2)) and $lang_path[2] === '_') {
-                # 如果查询得到属于语言包，则删除此路由
-                /**@var Locals $locals */
-                $locals = ObjectManager::getInstance(Locals::class);
-                $local = $locals->where(Locals::fields_CODE, $lang_path)
-                    ->where(Locals::fields_IS_INSTALL, 1)
-                    ->where(Locals::fields_IS_ACTIVE, 1)
-                    ->find()
-                    ->fetch();
-                if ($local) {
-                    $_SERVER['REQUEST_URI'] = $replace_part . '/' . implode('/', $uri_arr);
+                # 从缓存中获取语言包
+                $cache_key = 'i18n_langs';
+                /**@var I18nCache $cache */
+                $cache = ObjectManager::getInstance(I18nCache::class . 'Factory');
+                $langs = $cache->get($cache_key)?:[];
+                if ($langs) {
+                    # 如果查询得到属于语言包，则删除此路由
+                    $lang_pack_dir = Env::path_TRANSLATE_FILES_PATH;
+                    $langs_file    = glob($lang_pack_dir . '*.php');
+                    $langs         = [];
+                    foreach ($langs_file as $lang_file) {
+                        $langs[] = basename($lang_file, '.php');
+                    }
+                    $cache->set($cache_key, $langs, 3600 * 24 * 30);
+                } else {
+                    /**@var Locals $locals */
+                    $locals = ObjectManager::getInstance(Locals::class);
+                    $locals  = $locals
+                        ->where(Locals::fields_IS_INSTALL, 1)
+                        ->where(Locals::fields_IS_ACTIVE, 1)
+                        ->select()
+                        ->fetchOrigin();
+                    foreach ($locals as $local) {
+                        $langs[] = $local['code'];
+                    }
+                    $cache->set($cache_key, $langs, 3600 * 24 * 30);
+                }
+                if (in_array($lang_path, $langs)) {
+                    $_SERVER['REQUEST_URI']        = $replace_part . '/' . implode('/', $uri_arr);
                     $_SERVER['ORIGIN_REQUEST_URI'] = $uri;
                     Cookie::set('WELINE-USER-LANG', $lang_path, 3600 * 24 * 30);
                     $_SERVER['WELINE-USER-LANG'] = $lang_path;
